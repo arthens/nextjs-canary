ac960b4cde27799cee029c079a9e4b84b28110ea
Giacomo Gatelli <giacomo.gatelli@99designs.com>
2018-10-04 16:26:57 +1000

    Do not import a page from a page

--- features/welcome/index.jsx
+++ features/welcome/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic'
 
-const Hello = dynamic(() => import('../../features/Hello'))
+const Hello = dynamic(() => import('../hello'))
 
 export default () => (
 	<div>Welcome :) <Hello /></div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic'
 
-const Welcome = dynamic(() => import('./welcome'))
+const Welcome = dynamic(() => import('./../features/welcome'))
 
 export default () => (
 	<div><Welcome /></div>
